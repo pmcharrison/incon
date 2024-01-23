aa0bf23877503ac81340f36b6a2cbdf277cd5ba6
Peter Harrison <pmch2@cam.ac.uk>
2024-01-23 21:55:35 +0000

    Update Huron1994 doc

--- R/model-huron-1994.R
+++ R/model-huron-1994.R
@@ -1,21 +1,26 @@
+#' @export
 huron_1994 <- function(x) {
   UseMethod("huron_1994")
 }
 
+#' @export
 huron_1994.default <- function(x) {
   x <- hrep::pc_set(x)
   huron_1994.pc_set(x)
 }
 
+#' @export
 huron_1994.pc_set <- function(x) {
   x <- hrep::int_vec(x)
   huron_1994.int_vec(x)
 }
 
+#' @export
 huron_1994.int_vec <- function(x) {
   sum(x * huron_1994_weights)
 }
 
+#' @export
 huron_1994_weights <- c(- 1.428,
                         - 0.582,
                         0.594,
